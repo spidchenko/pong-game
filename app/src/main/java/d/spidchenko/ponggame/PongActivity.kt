0cf0a14d998a718c78d54832e3fcc6ed411337ae
Dmitry Spidchenko <spidchenko.d@gmail.com>
2022-06-03 19:14:41 +0300

    App now fullscreen. Removed unused dependencies

--- app/src/main/java/d/spidchenko/ponggame/PongActivity.kt
+++ app/src/main/java/d/spidchenko/ponggame/PongActivity.kt
@@ -1,11 +1,13 @@
 package d.spidchenko.ponggame
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.Window
 
-class MainActivity : AppCompatActivity() {
+class PongActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
     }
 }
